d73ec79f1eb545b6170ad2f3e37aa35a012c70c6
bergloman <bergloman@gmail.com>
2019-05-05 20:12:06 +0200

    Better regularization/normalization

--- src/bolts/bolt_regularizator.ts
+++ src/bolts/bolt_regularizator.ts
@@ -1,5 +1,5 @@
 import * as q from "./qtopology";
-import { ITsPointN } from "../data_objects";
+import { IMetricN } from "../data_objects";
 import { Regularizator, Normalizator } from "../tseries";
 
 export interface IRegularizatorBoltConfig extends q.IBoltAsyncConfig {
@@ -10,21 +10,26 @@ export interface IRegularizatorBoltConfig extends q.IBoltAsyncConfig {
 export class RegularizatorBolt implements q.IBoltAsync {
 
     private emit_cb: q.BoltAsyncEmitCallback;
-    private inner: Regularizator;
+    private delay: number;
+    private map: Map<string, Regularizator>;
 
     constructor() {
         this.emit_cb = null;
-        this.inner = null;
+        this.delay = 100;
+        this.map = new Map<string, Regularizator>();
     }
 
     public async init(_name: string, config: IRegularizatorBoltConfig, _context: any): Promise<void> {
         this.emit_cb = config.onEmit;
-        this.inner = new Regularizator(config.delay || 100, !!config.reemit_delay);
+        this.delay = config.delay || 100;
     }
 
     public async receive(data: any, _stream_id: string): Promise<void> {
-        const ddata: ITsPointN = data as ITsPointN;
-        const new_data = this.inner.add(ddata);
+        const ddata: IMetricN = data as IMetricN;
+        if (!this.map.has(ddata.name)) {
+            this.map.set(ddata.name, new Regularizator(this.delay || 100, false));
+        }
+        const new_data = this.map.get(ddata.name).add(ddata);
         for (const d of new_data) {
             const x = Object.assign({}, data);
             x.ts = d.ts;
@@ -45,21 +50,26 @@ export class RegularizatorBolt implements q.IBoltAsync {
 export class NormalizatorBolt implements q.IBoltAsync {
 
     private emit_cb: q.BoltAsyncEmitCallback;
-    private inner: Normalizator;
+    private delay: number;
+    private map: Map<string, Normalizator>;
 
     constructor() {
         this.emit_cb = null;
-        this.inner = null;
+        this.delay = 100;
+        this.map = new Map<string, Normalizator>();
     }
 
     public async init(_name: string, config: IRegularizatorBoltConfig, _context: any): Promise<void> {
         this.emit_cb = config.onEmit;
-        this.inner = new Normalizator(config.delay || 100, !!config.reemit_delay);
+        this.delay = config.delay || 100;
     }
 
     public async receive(data: any, _stream_id: string): Promise<void> {
-        const ddata: ITsPointN = data as ITsPointN;
-        const new_data = this.inner.add(ddata);
+        const ddata: IMetricN = data as IMetricN;
+        if (!this.map.has(ddata.name)) {
+            this.map.set(ddata.name, new Normalizator(this.delay || 100, false));
+        }
+        const new_data = this.map.get(ddata.name).add(ddata);
         for (const d of new_data) {
             const x = Object.assign({}, data);
             x.ts = d.ts;
